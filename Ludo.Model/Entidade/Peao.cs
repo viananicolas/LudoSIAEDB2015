796b4f0f51a48f4ee196215f7338b804c34b7f64
nicolasviana <viana.nicolas@outlook.com>
2015-02-26 01:25:04 -0300

    Esboçando ideias...

---
+++ Ludo.Model/Entidade/Peao.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ludo.Model.Entidade
+{
+
+    /*Devo inicializar os valores para cada peão de cada base. Ex: Peao Azul 1: Posição -1 [Na base], Restam 54 [Porque ele ainda não está nos quadrados
+     * nem na linha de chegada]. A classe deverá ser abstrada, com um construtor inicializando os valores quando um novo jogo for iniciado. 
+    */
+    public abstract class Peao
+    {
+        private bool bNovoJogo;
+        private string strCor;
+        private int inumBase;
+        private int[][] inumChegada;
+        private int[][] iblocosRestantes;
+
+        public string Cor { get { return strCor;} set; }
+
+        private static void InicializaValores(bool newGame)
+        {
+            if(newGame)
+            {
+                
+            }
+        }
+    }
+}
---
+++ Ludo.Model/Entidade/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ludo.Model.Entidade
+{
+    public abstract class Player
+    {
+        private string color;
+        private int thrownNumber;
+        private double positionX;
+        private double positionY;
+    }
+}
