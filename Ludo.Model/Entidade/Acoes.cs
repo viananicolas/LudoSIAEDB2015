84e16983797bbc84b4553796870734c484c2ea38
nicolasviana <viana.nicolas@outlook.com>
2015-03-03 18:15:54 -0300

    Fixing

---
+++ Ludo.Model/Entidade/Acoes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ludo.Model.Entidade
+{
+	public enum Acoes
+	{
+		Nada=0,
+		JogarDado=1,
+		MoverPeca=2,
+		EncerrarJogo=3
+	}
+}
---
+++ Ludo.Model/Entidade/Jogo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ludo.Model.Entidade
+{
+	public class Jogo
+	{
+		private int numTurnos;
+		private bool iniciaJogo;
+		private bool terminaJogo;
+		private List<Player> player;
+		public Player TurnoJogador;
+		private int turnojogadpr;
+		private bool jogouSeis;
+		public Acoes proximaAcao = Acoes.Nada;
+
+		public void ComecaJogo(List<Player> Jogadores)
+		{
+			this.player = Jogadores;
+			jogouSeis = false;
+			numTurnos = 1;
+			proximaAcao = Acoes.JogarDado;
+			jogouSeis = false;
+			TurnoJogador = this.player[0];
+		}
+		public void EncerrarJogo()
+		{
+			proximaAcao = Acoes.EncerrarJogo;
+		}
+
+		public void MoverPeca()
+		{
+
+		}
+		
+	}
+}
